49102ca058a19b5323e09c9fe699db1ea3eed9ef
dwinovic <nd.cahya19@gmail.com>
2021-09-26 19:37:07 +0700

    fixing update image

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,60 +1,30 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-const short = require('short-uuid');
 const maxSize = 1024 * 1024 * 2;
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const { configCloudinary } = require('./cloudinary');
 
-// START =  MULTIPLE UPLOAD
-// const storageMultiple = multer.diskStorage({
-//   destination: function(req, file, cb) {
-//     var dir = 'public/images';
-//     if (!fs.existsSync(dir)) {
-//       fs.mkdirSync(dir);
-//     }
-//     cb(null, dir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
+cloudinary.config(configCloudinary);
 
-// const multipleUpload = multer({
-//   storage: storageMultiple,
-//   limits: { fileSize: maxSize * 8 },
-//   fileFilter: function(req, file, cb) {
-//     checkFileType(file, cb);
-//   },
-// }).array('image', 8);
-
-// END =  MULTIPLE UPLOAD
-
-// START =  SINGLE UPLOAD
-// Set storage engine
-// const singleStorage = multer.diskStorage({
-//   // destination: 'public/images',
-//   storage: multer.diskStorage({}),
-//   filename: function(req, file, cb) {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'Teleclone',
+  },
+});
 
 const singleUpload = multer({
-  // storage: singleStorage,
-  storage: multer.diskStorage({}),
+  storage: storage,
   limits: { fileSize: maxSize },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   },
 }).single('avatar');
-// END =  SINGLE UPLOAD
 
-// // Check file Type
 function checkFileType(file, cb) {
-  // Allowed ext
   const fileTypes = /jpeg|jpg|png|gif/;
-  // Check ext
   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
   const mimeType = fileTypes.test(file.mimetype);
 
   if (mimeType && extName) {
